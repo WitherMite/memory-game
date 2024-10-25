13fc8fc8e9b5ff98115f861e56aaf7dded4f0722
WitherMite <43625516+WitherMite@users.noreply.github.com>
2024-10-25 17:04:20 -0500

    Planning

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 
+// get pokemon name/images from api (seems best to get whole pokemon list and keep in local storage); https://pokeapi.co/docs/v2#info
+
+// once no more visible unique cards, get max/some# new ones to replace randomly
+
+// use css to animate shuffles? would need to wait for animation before rerender during a shuffle somehow though...
+
+// MemoryGame(has to contain state/effects since all reactive components will need them and the gamestate is interconnected) >
+//   Title + Scoreboard + CardTable >
+//     Card
 createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
